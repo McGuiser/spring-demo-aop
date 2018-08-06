a20719e5853d4f3fe79538cc78cd654c366907ef
Corey Castillo <mcguiser@users.noreply.github.com>
2018-08-06 15:38:35 -0500

    AOP - Pointcut Expressions - Match any addAccount Method
    Created Membership DAO with an addAccount method,
    Modified MainDemoApp.

--- src/com/corey/aopdemo/MainDemoApp.java
+++ src/com/corey/aopdemo/MainDemoApp.java
@@ -3,6 +3,7 @@ package com.corey.aopdemo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import com.corey.aopdemo.dao.AccountDAO;
+import com.corey.aopdemo.dao.MembershipDAO;
 
 public class MainDemoApp {
 
@@ -15,8 +16,15 @@ public class MainDemoApp {
 		// Get the bean from spring container
 		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
 		
+		// Get membership bean from spring container
+		MembershipDAO theMembershipDAO = 
+				context.getBean("membershipDAO", MembershipDAO.class);
+		
 		// Call the business method
 		theAccountDAO.addAccount();
+		
+		// Call the membership business method
+		theMembershipDAO.addAccount();
 		
 		// Close the context
 		context.close();
